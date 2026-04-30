2544baa3a9504570d608f2589ad5573a74e2626d
Leroy0824 <kinged.1234567890@gmail.com>
2026-04-30 15:36:30 +0100

    Add files via upload

---
+++ Figure 3.R
@@ -0,0 +1,8 @@
+x<-seq(0,50,by=0.01)
+y<-pnorm((2.32625-1.95996)/x)
+plot(x,y,type="l",xlab="Replication noise",ylab="Reproducible probability")
+abline(h=0.5,col="Red")
+legend("topright",
+       legend=c("s=1","p=0.5"),
+       col=c("red","black"),
+       lty=c(1,1))
